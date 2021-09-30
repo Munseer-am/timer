15289d4a46bbf427448f046eb364e5dae092bb6d
Munseer.am <82161602+Anonymous1829@users.noreply.github.com>
2021-09-30 12:37:59 +0530

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -3,15 +3,18 @@
 """This timer only supports seconds format."""
 __author__ = "Munseer"
 
+# importing required modules
 import time
 import sys
 
+# printing name of author
 print("""  __  __
  |  \/  |_   _ _ __  ___  ___  ___ _ __
  | |\/| | | | | '_ \/ __|/ _ \/ _ \ '__|
  | |  | | |_| | | | \__ \  __/  __/ |
  |_|  |_|\__,_|_| |_|___/\___|\___|_|""")
 
+# codes for timer 
 def countdown(t):
 	while t:
 		mins,secs = divmod(t,60)
@@ -21,8 +24,11 @@ def countdown(t):
 		time.sleep(1)
 		t -= 1
 	print(' time up')
-	
+
+# user input	
 t = int(input(' Enter time in seconds: '))
+
+# time limit
 if t > 3600:
 	sys.exit()
 else:
